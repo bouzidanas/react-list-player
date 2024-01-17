a143d75cd26a5ebd6a422bb3e371c675309ffb19
Anas Bouzid <anasbouzid@gmail.com>
2024-01-17 02:48:31 -0600

    add package exports

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { ListPlayer, ListInfoCard, Header } from "./ListPlayer";
+import type { track, listInfo } from "./ListPlayer";
+
+export { ListPlayer, ListInfoCard, Header };
+export type { track, listInfo };
