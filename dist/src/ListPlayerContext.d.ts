17de7fbb605393b93374a41c906846bc95862e1c
Anas Bouzid <anasbouzid@gmail.com>
2024-01-18 08:29:43 -0600

    update build

--- dist/src/ListPlayerContext.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export declare const ListPlayerContext: import("react").Context<{
-    selectedTrack: number;
-    isPlaying: boolean;
-    isMuted: boolean;
-    setSelectedTrack: (track: number) => void;
-    setIsPlaying: (isPlaying: boolean) => void;
-    setIsMuted: (isMuted: boolean) => void;
-}>;
