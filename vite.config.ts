717284e7027911e533e14578a0d10368b1b4efbe
Anas Bouzid <anasbouzid@gmail.com>
2024-01-18 08:28:55 -0600

    undo package prep

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
-import dts from 'vite-plugin-dts';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/guide/build.html#library-mode
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'index.ts'),
-      name: 'rpp',
-      fileName: 'rpp',
-    },
-  },
-  plugins: [dts()],
-});
+  plugins: [react()],
+})
