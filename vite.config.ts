4f72a85fc85d0b3a1d48f56454b7ac065bfc6f48
Anas Bouzid <anasbouzid@gmail.com>
2024-01-18 18:52:37 -0600

    prep package and build for prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'rpp',
+      // the proper extensions will be added
+      fileName: 'rpp',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+     // output: {
+     //   // Provide global variables to use in the UMD build
+     //   // for externalized deps
+     //   globals: {
+     //   react: 'React',
+     //   },
+     // },
+    },
+  },
 })
