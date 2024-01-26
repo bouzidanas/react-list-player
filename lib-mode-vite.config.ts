8227afccad47b8cb9bd2289bb23f2e4034fb1f65
Anas Bouzid <anasbouzid@gmail.com>
2024-01-26 05:31:50 -0600

    remove redundant exports

--- lib-mode-vite.config.ts
+++ lib-mode-vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     copyPublicDir: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'index.ts'),
       name: 'rlp',
       // the proper extensions will be added
       fileName: 'rlp',
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Demo } from "./Demo";
-
-import { ListPlayerContext } from "./ListPlayerContext";
-import { ListPlayer, ListControls, ListHeader, ListInfoCard } from "./ListPlayer";
-import type { listInfo, track } from "./ListPlayer";
-
-export { Demo, ListPlayerContext, ListPlayer, ListControls, ListHeader, ListInfoCard };
-export type { listInfo, track };
-
